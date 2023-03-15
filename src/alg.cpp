2f693dd4546d8e6fd0d7abd473a7c84d64f8fea4
olesyakomarova <116936462+olesyakomarova@users.noreply.github.com>
2023-03-15 21:19:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int leftgran = 0;
+  int rightgran = size - 1;
+  while (leftgran <= rightgran) {
+    int sered = (leftgran + rightgran) / 2;
+      if (arr[sered] == value) {
+      count++;
+      int i = sered - 1;
+      while (i >= leftgran && arr[i] == value) {
+        count++;
+        i--;
+      }
+      i = sered + 1;
+      while (i <= rightgran && arr[i] == value) {
+        count++;
+        i++;
+      }
+      break;
+    } else if (arr[sered] < value) {
+      leftgran = sered + 1;
+    } else {
+      rightgran = sered - 1;
+    }
+  }
+  return count;
 }
+
